c03b0d073ecc73125332b0e38a204da82c6cde4a
hunter-funk <hunter.d.funk@gmail.com>
2024-11-04 19:27:36 -0700

    Overlay front-end working as expected
    Still need to program the buttons, but that has to wait until the other windows are made.

--- LoftGolfOverlayUI/LoftGolfOverlayUI/Program.cs
+++ LoftGolfOverlayUI/LoftGolfOverlayUI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace LoftGolfOverlayUI
 {
     internal static class Program
@@ -14,16 +16,23 @@ namespace LoftGolfOverlayUI
             Application.Run(new Form1());
             /* 
              * TO DO:
-             *      -ADD AN ARROW TO THE TOP PORTION OF OVERLAY TO INDICATE THE MENU CAN SLIDE UP
-             *      -OnMouseEnter() INTO OVERLAY
+             *     O -ADD AN ARROW TO THE TOP PORTION OF OVERLAY TO INDICATE THE MENU CAN SLIDE UP
+             *          -- ADD ICONS FOR ALL BUTTONS AS WELL
+             *          --- DONE !!!
+             *     O - ON STARTUP SET POSITION OF OVERLAY
+             *          -- CHANGE TRANSFORM OF THE FORM TO SIT IN THE MIDDLE BOTTOM OF THE SCREEN
+             *          --- DONE !!! *see form1.cs form1 constructor
+             *     X -OnMouseEnter() INTO OVERLAY
              *          -- INCREASE FORM OPACITY TO 100%
              *          -- CHANGE TRANSFORM OF THE FORM TO SLIDE UP
-             *      -OnMouseExit() FROM OVERLAY
+             *     X -OnMouseExit() FROM OVERLAY
              *          -- DECREASE FORM OPACITY TO 80%
              *          -- CHANGE TRANSFORM OF THE FORM TO SLIDE DOWN
-             *      -KEEP FORM ON SCREEN ABOVE EVERYTHING
-             *      -PROGRAM BUTTONS
+             *     X -KEEP FORM ON SCREEN ABOVE EVERYTHING
+             *     X -PROGRAM BUTTONS
              */
         }
+
+        
     }
 }
