2093826820b513e81d62b402f18e643bba657bdf
hunter-funk <hunter.d.funk@gmail.com>
2025-02-02 15:03:26 -0700

    updated interface and added caution form when switching
    changed overlay interface to be labels instead of buttons, added effects when the mouse enters each one, added a caution form when the user attempts to navigate elsewehere.

--- LoftGolfOverlayUI/LoftGolfOverlayUI/Form1.cs
+++ LoftGolfOverlayUI/LoftGolfOverlayUI/Form1.cs
@@ -49,11 +49,97 @@ namespace LoftGolfOverlayUI
             UI_Clock.Text = DateTime.Now.ToString("hh:mm:ss tt");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void goHome()
         {
             Form2 form2 = new Form2();
 
             form2.Show();
+
+            this.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            caution("home");
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            caution("home");
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void golf_hover(object sender, EventArgs e)
+        {
+            label8.BackgroundImage = Properties.Resources.golf_highlight1; // Make sure to add your image to Resources
+            label8.BackgroundImageLayout = ImageLayout.Stretch; // Optional: Set how the image should fit
+        }
+
+        private void golf_leave(object sender, EventArgs e)
+        {
+            label8.BackgroundImage = null;
+        }
+        private void home_hover(object sender, EventArgs e)
+        {
+            label7.BackgroundImage = Properties.Resources.system_highlight1; // Make sure to add your image to Resources
+            label7.BackgroundImageLayout = ImageLayout.Stretch; // Optional: Set how the image should fit
+        }
+
+        private void home_leave(object sender, EventArgs e)
+        {
+            label7.BackgroundImage = null;
+        }
+        private void karaoke_hover(object sender, EventArgs e)
+        {
+            label9.BackgroundImage = Properties.Resources.karaoke_highlight1; // Make sure to add your image to Resources
+            label9.BackgroundImageLayout = ImageLayout.Stretch; // Optional: Set how the image should fit
+        }
+
+        private void karaoke_leave(object sender, EventArgs e)
+        {
+            label9.BackgroundImage = null;
+        }
+        private void movie_hover(object sender, EventArgs e)
+        {
+            label10.BackgroundImage = Properties.Resources.movie_highlight1; // Make sure to add your image to Resources
+            label10.BackgroundImageLayout = ImageLayout.Stretch; // Optional: Set how the image should fit
+        }
+
+        private void movie_leave(object sender, EventArgs e)
+        {
+            label10.BackgroundImage = null;
+        }
+        private void meeting_hover(object sender, EventArgs e)
+        {
+            label11.BackgroundImage = Properties.Resources.meeting_highlight1; // Make sure to add your image to Resources
+            label11.BackgroundImageLayout = ImageLayout.Stretch; // Optional: Set how the image should fit
+        }
+
+        private void meeting_leave(object sender, EventArgs e)
+        {
+            label11.BackgroundImage = null;
+        }
+
+        private void system_hover(object sender, EventArgs e)
+        {
+            label12.BackgroundImage = Properties.Resources.system_highlight1; // Make sure to add your image to Resources
+            label12.BackgroundImageLayout = ImageLayout.Stretch; // Optional: Set how the image should fit
+        }
+
+        private void system_leave(object sender, EventArgs e)
+        {
+            label12.BackgroundImage = null;
+        }
+
+        private void caution(string destination)
+        {
+            Form3 form3 = new Form3(destination);
+
+            form3.Show();
 
             this.Hide();
         }
---
+++ LoftGolfOverlayUI/LoftGolfOverlayUI/Form3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoftGolfOverlayUI
+{
+    public partial class Form3 : Form
+    {
+        string destination;
+        public Form3(string destination)
+        {
+            InitializeComponent();
+            this.destination = destination;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+
+            form1.Show();
+
+            this.Hide();
+        }
+
+        private void displayOverlay()
+        {
+            Form1 form1 = new Form1();
+
+            form1.Show();
+
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (destination){
+                case "home":
+                    Form2 form2 = new Form2();
+                    form2.Show();
+                    this.Hide();
+                    break;
+                case "golf":
+                    // run autohotkey stuff for golf
+                    displayOverlay();
+                    break;
+                case "karaoke":
+                    // run autohotkey stuff for karaoke
+                    displayOverlay();
+                    break;
+                case "movie":
+                    // run autohotkey stuff for movies
+                    displayOverlay();
+                    break;
+                case "meeting":
+                    // run autohotkey stuff for meeting
+                    displayOverlay();
+                    break;
+                    
+            };
+                
+        }
+    }
+}
