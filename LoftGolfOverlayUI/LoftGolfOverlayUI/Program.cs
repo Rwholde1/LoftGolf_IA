48d8d497a22561b25c2dc4c9f806fd86160c4cad
hunter-funk <hunter.d.funk@gmail.com>
2025-02-09 19:07:01 -0700

    Added new pages and tracking current activity
    Added pages for a user trying to access page under construction, as well as if they try to navigate to a page they're already in. Next time, work on getting forms centered when they spawn in, as well as preventing the user from spawning multiple construction pages

--- LoftGolfOverlayUI/LoftGolfOverlayUI/Program.cs
+++ LoftGolfOverlayUI/LoftGolfOverlayUI/Program.cs
@@ -13,7 +13,7 @@ namespace LoftGolfOverlayUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form2());
             /* 
              * TO DO:
              *     O -ADD AN ARROW TO THE TOP PORTION OF OVERLAY TO INDICATE THE MENU CAN SLIDE UP
